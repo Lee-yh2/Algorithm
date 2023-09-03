3552a3e419970b04cc0adf91774e97c85c3b5263
Lee-yh2 <dbsgud101@naver.com>
2023-09-03 16:38:46 +0900

    해커스랭크 birthdayCakeCandles 풀이

---
+++ src/algolithm/programmers/random/숫자카드나누기.java
@@ -0,0 +1,14 @@
+package algolithm.programmers.random;
+
+
+import java.util.*;
+
+public class 숫자카드나누기 {
+    public static void main(String[] args) {
+        solution();
+    }
+    public static int solution() {
+        int answer = 0;
+        return answer;
+    }
+}
